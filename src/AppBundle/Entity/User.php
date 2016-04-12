e80dc50e5abff68fec569f18fda09d3a3f79286e
Michał Kurzeja <mike.kurzeja@gmail.com>
2016-04-12 13:45:13 +0200

    Lib updates and FOSUserBundle

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,153 +2,24 @@
 
 namespace AppBundle\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 
 /**
- * @ORM\Table(name="users")
- * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
- * @UniqueEntity(fields="email", message="user.email.taken")
- * @UniqueEntity(fields="username", message="user.username.taken")
+ * @ORM\Entity
+ * @ORM\Table(name="app_user")
  */
-class User implements AdvancedUserInterface, \Serializable
+class User extends BaseUser
 {
     /**
-     * @ORM\Column(type="integer")
      * @ORM\Id
+     * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
-
-    /**
-     * @ORM\Column(type="string", length=25, unique=true)
-     * @Assert\NotBlank(groups={"default", "registration"})
-     */
-    private $username;
-
-    /**
-     * @ORM\Column(type="string", length=64)
-     * @Assert\NotBlank()
-     */
-    private $password;
-
-    /**
-     * @Assert\NotBlank(groups="registration")
-     * @Assert\Length(max=4096)
-     */
-    private $plainPassword;
-
-    /**
-     * @ORM\Column(type="string", length=60, unique=true)
-     * @Assert\NotBlank(groups={"default", "registration"})
-     * @Assert\Email()
-     */
-    private $email;
-
-    /**
-     * @ORM\Column(type="boolean")
-     * @Assert\NotBlank()
-     */
-    private $active;
+    protected $id;
 
     public function __construct()
     {
-        $this->active = true;
-    }
-
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    public function setUsername($username)
-    {
-        $this->username = $username;
-    }
-
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
-
-    public function getSalt()
-    {
-        return null;
-    }
-
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    public function setPassword($password)
-    {
-        $this->password = $password;
-    }
-
-    public function getPlainPassword()
-    {
-        return $this->plainPassword;
-    }
-
-    public function setPlainPassword($plainPassword)
-    {
-        $this->plainPassword = $plainPassword;
-    }
-
-    public function getRoles()
-    {
-        return array('ROLE_USER');
-    }
-
-    public function eraseCredentials()
-    {
-    }
-
-    public function isAccountNonExpired()
-    {
-        return true;
-    }
-
-    public function isAccountNonLocked()
-    {
-        return true;
-    }
-
-    public function isCredentialsNonExpired()
-    {
-        return true;
-    }
-
-    public function isEnabled()
-    {
-        return $this->active;
-    }
-
-    public function serialize()
-    {
-        return serialize([
-            $this->id,
-            $this->username,
-            $this->password,
-            $this->active,
-        ]);
-    }
-
-    public function unserialize($serialized)
-    {
-        list (
-            $this->id,
-            $this->username,
-            $this->password,
-            $this->active,
-        ) = unserialize($serialized);
+        parent::__construct();
     }
 }
